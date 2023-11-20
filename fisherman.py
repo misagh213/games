b4795de3acdc75b4109c9cecd5f3a76531fc2014
Misagh <149416988+misagh213@users.noreply.github.com>
2023-11-19 20:17:29 -0800

    Add files via upload

---
+++ fisherman.py
@@ -0,0 +1,10 @@
+Weightـpounds = int(input("Enter value"))
+
+Weightـkg = Weightـpounds * 1
+
+price = Weightـkg * 2
+
+if price >= 60:
+    print("shoma barane ghlab shodid")
+elif price < 60:
+    print("shoma farda hagh kar kardan nadarid")
